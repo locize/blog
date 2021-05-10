4404c9be750d8305dcfecafcc34dc74eeed07b86
Adriano Raiano <adriano@raiano.ch>
2021-05-10 21:31:30 +0200

    update

---
+++ scripts/no-cookie-youtube.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { htmlTag } = require('hexo-util');
+
+/**
+* Youtube tag
+*
+* Syntax:
+*   {% youtube video_id, type, cookie %}
+*/
+function youtubeTag([id, type = 'video', cookie = true]) {
+  if (typeof type === 'boolean') {
+    cookie = type;
+    type = 'video';
+  }
+
+  // const ytLink = cookie ? 'https://www.youtube.com' : 'https://www.youtube-nocookie.com';
+  const ytLink = 'https://www.youtube-nocookie.com';
+  const embed = type === 'video' ? '/embed/' : '/embed/videoseries?list=';
+
+  const iframeTag = htmlTag('iframe', {
+    src: ytLink + embed + id,
+    frameborder: '0',
+    loading: 'lazy',
+    allowfullscreen: true
+  }, '');
+
+  return htmlTag('div', { class: 'video-container' }, iframeTag, false);
+}
+
+// https://hexo.io/docs/tag-plugins#YouTube
+hexo.extend.tag.unregister('youtube');
+
+hexo.extend.tag.register('youtube', youtubeTag);
