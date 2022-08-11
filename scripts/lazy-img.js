d3d256cebff63f0630f88ec049414ec1fc0694d5
Adriano Raiano <adriano@raiano.ch>
2022-08-11 11:23:51 +0200

    update

--- scripts/lazy-img.js
+++ scripts/lazy-img.js
@@ -1,4 +1,9 @@
-hexo.extend.filter.register('after_render:html', function(data) {
-  data = data.replace(/<img /g, '<img loading="lazy" ');
+hexo.extend.filter.register('after_render:html', function(data, value) {
+  if (value && value.page && value.page.thumbnail && value.page.thumbnail.indexOf('/') > 0) {
+    const thumpImg = value.page.thumbnail.substring(value.page.thumbnail.indexOf('/') + 1)
+    data = data.replace(new RegExp(`(?!<img src="${thumpImg}")<img `, 'g'), '<img loading="lazy" ');
+  } else {
+    data = data.replace(/<img /g, '<img loading="lazy" ');
+  }
   return data;
 });
